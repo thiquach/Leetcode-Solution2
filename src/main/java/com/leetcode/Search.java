2d911aa4f96c97ed33b7496a735f2500d14f7845
thiquach <amylyquach>
2025-03-15 16:50:23 +1100

    Initial commit

---
+++ src/main/java/com/leetcode/Search.java
@@ -0,0 +1,13 @@
+package main.java.com.leetcode;
+
+public class Search {
+    public static void main(String[] args) {
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 11;
+        System.out.println(" search " + search(nums, target));
+    }
+
+    public static int search(int[] nums, int target) {
+        return -1;
+    }
+}
