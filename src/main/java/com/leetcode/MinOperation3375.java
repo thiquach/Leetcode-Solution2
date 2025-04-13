6f2577f46306c9ec94df3c1464bea1741512fa98
thiquach <amylyquach>
2025-04-13 11:32:26 +1000

    update min operation 3375 using HashMap

--- src/main/java/com/leetcode/MinOperation3375.java
+++ src/main/java/com/leetcode/MinOperation3375.java
@@ -2,34 +2,30 @@ package main.java.com.leetcode;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MinOperation3375 {
     public static void main(String[] args) {
-//        int[] nums = {5,2,5,4,5}; // k=2
+        int[] nums = {5,2,5,4,5}; // k=2
 //        int[] nums = {9,7,5,3};
-        int[] nums = {1};
-        int k = 1;
+//        int[] nums = {1};
+        int k = 2;
         System.out.println(" minOps " + minOperations(nums, k));
     }
 
     public static int minOperations(int[] nums, int k) {
-        Integer lastLargest = 0;
-        int index = 0;
-        int count = 0;
-        for (int n: nums) {
-            Integer nextLargest = Arrays.stream(nums)
-                    .boxed()
-                    .sorted(Comparator.reverseOrder())
-                    .skip(index++)
-                    .findFirst().get();
-            if (!lastLargest.equals(nextLargest)) {
-                lastLargest = nextLargest;
-                if (nextLargest > k)
-                    count++;
-            }
+
+        Map<Integer, Integer> numsMap = new HashMap<>();
+
+        for (int n:nums) {
             if (n < k)
                 return -1;
+
+            if (n > k)
+                numsMap.put(n, numsMap.getOrDefault(n, 0) + 1);
         }
-        return count;
+
+        return numsMap.size();
     }
 }
