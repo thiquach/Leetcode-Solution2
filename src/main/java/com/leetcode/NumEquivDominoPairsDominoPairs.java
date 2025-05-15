4f8efb853a2142fe6e85fae77425b7e5de4ff8f9
thiquach <amylyquach>
2025-05-15 10:19:36 +1000

    add number equivalent domino pairs

--- src/main/java/com/leetcode/NumEquivDominoPairsDominoPairs.java
+++ src/main/java/com/leetcode/NumEquivDominoPairsDominoPairs.java
@@ -1,2 +1,34 @@
-package main.java.com.leetcode;public class DominoPairs {
+package main.java.com.leetcode;
+
+import javafx.util.Pair;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NumEquivDominoPairsDominoPairs {
+    public static void main(String[] args) {
+//        int[][] dominoes = {{1,2},{2,1}, {3,4}, {5,6}};
+//        int[][] dominoes = {{1,2},{1,2},{1,1},{1,2}, {2,2}};
+        int[][] dominoes = {{2,1},{1,2},{1,2},{1,2},{2,1},{1,1},{1,2},{2,2}};
+        System.out.println("num equiv domino pairs " + numEquivDominoPairs(dominoes));
+    }
+
+    public static int numEquivDominoPairs(int[][] dominoes) {
+        int[] pair = new int[100];
+        for (int i=0; i<dominoes.length; i++) {
+            int a = dominoes[i][0];
+            int b = dominoes[i][1];
+            int key = (a < b) ? (a*10 + b) : (b*10 + a);
+            pair[key]++;
+        }
+
+        int pairCount = 0;
+        for (int freq: pair) {
+            if (freq > 1) {
+                pairCount += (freq * (freq - 1))/2;
+            }
+        }
+
+        return pairCount;
+    }
 }
