407aa4aa4b3a8eee9f5a37a13b136e4db08daa47
thiquach <amylyquach>
2025-05-15 10:40:30 +1000

    add number of equivalent domino pairs

--- src/main/java/com/leetcode/NumEquivDominoPairs.java
+++ src/main/java/com/leetcode/NumEquivDominoPairs.java
@@ -1,11 +1,6 @@
 package main.java.com.leetcode;
 
-import javafx.util.Pair;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class NumEquivDominoPairsDominoPairs {
+public class NumEquivDominoPairs {
     public static void main(String[] args) {
 //        int[][] dominoes = {{1,2},{2,1}, {3,4}, {5,6}};
 //        int[][] dominoes = {{1,2},{1,2},{1,1},{1,2}, {2,2}};
